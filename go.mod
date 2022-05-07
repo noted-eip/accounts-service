e4ee423cba7827557bb459ded0c9c286be081238
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-05-07 13:35:46 +0200

    feat: environment and port flag

--- go.mod
+++ go.mod
@@ -11,15 +11,24 @@ require (
 )
 
 require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	go.uber.org/zap v1.21.0
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"net"
+	"os"
 
 	"accounts-service/auth"
 	"accounts-service/grpc/accountspb"
 
 	_ "github.com/joho/godotenv/autoload"
-
 	"google.golang.org/grpc"
-
 	"google.golang.org/grpc/reflection"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	app = kingpin.New("accounts-service", "Accounts service for the Noted backend").DefaultEnvars()
+
+	port        = app.Flag("port", "grpc server port").Default("3000").Int16()
+	environment = app.Flag("environment", "either development or production").Default("development").String()
 )
 
 func main() {
+	app.Parse(os.Args[1:])
+
 	srv := grpc.NewServer(
 		grpc.UnaryInterceptor(auth.ForwardAuthMetadatathUnaryInterceptor),
 	)
@@ -21,11 +31,14 @@ func main() {
 	accSrv := accountsService{}
 	accountspb.RegisterAccountsServiceServer(srv, &accSrv)
 
-	lis, err := net.Listen("tcp", ":50051")
+	startServer(srv)
+}
+
+func startServer(srv *grpc.Server) {
+	lis, err := net.Listen("tcp", fmt.Sprint(":", *port))
 	if err != nil {
 		panic(err)
 	}
-
 	reflection.Register(srv)
 	if err := srv.Serve(lis); err != nil {
 		panic(err)
