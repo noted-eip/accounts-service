343166e3fa12dd166dcac3777ab91ecfb5c45918
Maxime Dodin <maxime.dodin@epitech.eu>
2022-05-09 22:17:55 +0200

    feat(Controllers): delete, get, and create Accounts operation, with validators for account creation info and password hash

--- accounts.go
+++ accounts.go
@@ -3,7 +3,15 @@ package main
 import (
 	"accounts-service/auth"
 	"accounts-service/grpc/accountspb"
+	"accounts-service/models"
+
+	val "accounts-service/validators"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+
 	"context"
+	"log"
 
 	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
@@ -18,34 +26,103 @@ type accountsService struct {
 	logger *zap.SugaredLogger
 }
 
-var _ accountspb.AccountsServiceServer = &accountsService{}
-
-func (srv *accountsService) CreateAccount(ctx context.Context, in *accountspb.Account) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+type Account struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Email    string             `bson:"email,omitempty" json:"email,omitempty"`
+	Name     string             `bson:"name,omitempty" json:"name,omitempty"`
+	Password []byte             `bson:"password,omitempty"`
 }
 
+type MongoId struct {
+	ID primitive.ObjectID `bson:"_id,omitempty"`
+}
+
+var _ accountspb.AccountsServiceServer = &accountsService{}
+
+// Create an Account from username, password and email
+func (srv *accountsService) CreateAccount(ctx context.Context, in *accountspb.CreateAccountRequest) (*emptypb.Empty, error) {
+	errValidator := val.ValidateAccountCreation(in)
+	if errValidator != nil {
+		log.Print("[ERR] ", errValidator.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errValidator.Error())
+	}
+
+	collection := models.UsersDatabase.Client().Database("Users").Collection("Accounts")
+
+	// hash password for storage
+	hashed, errHash := bcrypt.GenerateFromPassword([]byte(in.Password), 8)
+	if errHash != nil {
+		log.Print("[ERR] ", errHash.Error())
+		return nil, status.Errorf(codes.Internal, errHash.Error())
+	}
+
+	_, err := collection.InsertOne(context.TODO(), Account{Email: in.Email, Name: in.Name, Password: hashed})
+	if err != nil {
+		log.Print("[ERR] ", err.Error())
+		return nil, status.Errorf(codes.Internal, err.Error())
+	}
+
+	return nil, status.Errorf(codes.OK, "")
+}
+
+// Return Account associate to specify Id
 func (srv *accountsService) GetAccount(ctx context.Context, in *accountspb.GetAccountRequest) (*accountspb.Account, error) {
 	_, err := srv.authenticate(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+	var account Account
+	_id, errIdFromHex := primitive.ObjectIDFromHex(in.Id)
+	if errIdFromHex != nil {
+		log.Print("[ERR] ", errIdFromHex.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errIdFromHex.Error())
+	}
+
+	errFind := models.UsersDatabase.Client().Database("Users").Collection("Accounts").FindOne(context.TODO(), MongoId{_id}).Decode(&account)
+	if errFind != nil {
+		log.Print("[ERR] ", errFind.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errFind.Error())
+	}
+
+	return &accountspb.Account{Email: account.Email, Name: account.Name, Id: account.ID.Hex()}, nil
 }
 
+// In Progress
 func (srv *accountsService) UpdateAccount(ctx context.Context, in *accountspb.UpdateAccountRequest) (*accountspb.Account, error) {
 	_, err := srv.authenticate(ctx)
 	if err != nil {
 		return nil, err
 	}
 
+	/*
+		fieldMask := in.GetUpdateMask()
+		err := fieldMask.IsValid(in.Account)
+		if err != false {
+			log.Print("[ERR]")
+			return nil, status.Errorf(codes.InvalidArgument, "")
+		}
+	*/
 	return nil, status.Errorf(codes.Unimplemented, "not implemented")
 }
 
+// Delete Account associate to specify Id
 func (srv *accountsService) DeleteAccount(ctx context.Context, in *accountspb.DeleteAccountRequest) (*emptypb.Empty, error) {
 	_, err := srv.authenticate(ctx)
 	if err != nil {
 		return nil, err
+	}
+
+	_id, errIdFromHex := primitive.ObjectIDFromHex(in.Id)
+	if errIdFromHex != nil {
+		log.Print("[ERR] ", errIdFromHex.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errIdFromHex.Error())
+	}
+
+	_, errDelete := models.UsersDatabase.Client().Database("Users").Collection("Accounts").DeleteOne(context.TODO(), MongoId{_id})
+	if errDelete != nil {
+		log.Print("[ERR] ", errDelete.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errDelete.Error())
 	}
 
 	return nil, status.Errorf(codes.Unimplemented, "not implemented")
--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module accounts-service
 go 1.18
 
 require (
+	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
+	github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 	github.com/google/uuid v1.1.2
 	github.com/joho/godotenv v1.4.0
 	github.com/stretchr/testify v1.7.1
+	go.mongodb.org/mongo-driver v1.9.1
+	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
 )
@@ -15,13 +19,22 @@ require (
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/zap v1.21.0
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
