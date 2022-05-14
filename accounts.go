2733682ed4c97725d5b89e4274004c7ea83980c2
Maxime Dodin <maxime.dodin@epitech.eu>
2022-05-14 17:46:01 +0200

    feat: implement update request taking update_mask and updating specify account propertie(s)

--- accounts.go
+++ accounts.go
@@ -7,15 +7,21 @@ import (
 
 	val "accounts-service/validators"
 
+	"github.com/mennanov/fmutils"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 
 	"context"
 	"log"
 
+	"github.com/jinzhu/copier"
 	"go.uber.org/zap"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
@@ -30,7 +36,7 @@ type Account struct {
 	ID       primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
 	Email    string             `bson:"email,omitempty" json:"email,omitempty"`
 	Name     string             `bson:"name,omitempty" json:"name,omitempty"`
-	Password []byte             `bson:"password,omitempty"`
+	Password []byte             `bson:"password,omitempty" json:"password,omitempty"`
 }
 
 type MongoId struct {
@@ -50,7 +56,7 @@ func (srv *accountsService) CreateAccount(ctx context.Context, in *accountspb.Cr
 	collection := models.UsersDatabase.Client().Database("Users").Collection("Accounts")
 
 	// hash password for storage
-	hashed, errHash := bcrypt.GenerateFromPassword([]byte(in.Password), 8)
+	hashed, errHash := bcrypt.GenerateFromPassword([]byte(in.GetPassword()), 8)
 	if errHash != nil {
 		log.Print("[ERR] ", errHash.Error())
 		return nil, status.Errorf(codes.Internal, errHash.Error())
@@ -62,7 +68,7 @@ func (srv *accountsService) CreateAccount(ctx context.Context, in *accountspb.Cr
 		return nil, status.Errorf(codes.Internal, err.Error())
 	}
 
-	return nil, status.Errorf(codes.OK, "")
+	return new(emptypb.Empty), nil
 }
 
 // Return Account associate to specify Id
@@ -88,22 +94,49 @@ func (srv *accountsService) GetAccount(ctx context.Context, in *accountspb.GetAc
 	return &accountspb.Account{Email: account.Email, Name: account.Name, Id: account.ID.Hex()}, nil
 }
 
-// In Progress
+// Update accounts from updateMask
 func (srv *accountsService) UpdateAccount(ctx context.Context, in *accountspb.UpdateAccountRequest) (*accountspb.Account, error) {
 	_, err := srv.authenticate(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	/*
-		fieldMask := in.GetUpdateMask()
-		err := fieldMask.IsValid(in.Account)
-		if err != false {
-			log.Print("[ERR]")
-			return nil, status.Errorf(codes.InvalidArgument, "")
-		}
-	*/
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+	var protoAccount accountspb.Account
+	var account Account
+
+	fieldMask := in.GetUpdateMask()
+	fieldMask.Normalize()
+	if !fieldMask.IsValid(in.Account) {
+		log.Print("[ERR] invalid update mask")
+		return nil, status.Errorf(codes.InvalidArgument, "")
+	}
+
+	_id, errIdFromHex := primitive.ObjectIDFromHex(in.GetAccount().GetId())
+	if errIdFromHex != nil {
+		log.Print("[ERR] ", errIdFromHex.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errIdFromHex.Error())
+	}
+
+	fmutils.Filter(in.GetAccount(), fieldMask.GetPaths())
+
+	errFind := models.UsersDatabase.Client().Database("Users").Collection("Accounts").FindOne(context.TODO(), MongoId{_id}).Decode(&protoAccount)
+	if errFind != nil {
+		log.Print("[ERR] ", errFind.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errFind.Error())
+	}
+
+	// Now that the request is vetted we can merge it with the account entity.
+	proto.Merge(&protoAccount, in.GetAccount())
+	// Exclude id variable from account message to Account schema (id != _id).
+	copier.Copy(&account, &protoAccount)
+	// The User can now be saved in a database.
+	_, errUpdate := models.UsersDatabase.Client().Database("Users").Collection("Accounts").UpdateOne(ctx, MongoId{_id}, bson.D{{Key: "$set", Value: &account}})
+	if errUpdate != nil {
+		log.Print("[ERR] ", errUpdate.Error())
+		return nil, status.Errorf(codes.InvalidArgument, errUpdate.Error())
+	}
+
+	return &accountspb.Account{Email: account.Email, Name: account.Name, Id: account.ID.String()}, nil
 }
 
 // Delete Account associate to specify Id
@@ -113,7 +146,7 @@ func (srv *accountsService) DeleteAccount(ctx context.Context, in *accountspb.De
 		return nil, err
 	}
 
-	_id, errIdFromHex := primitive.ObjectIDFromHex(in.Id)
+	_id, errIdFromHex := primitive.ObjectIDFromHex(in.GetId())
 	if errIdFromHex != nil {
 		log.Print("[ERR] ", errIdFromHex.Error())
 		return nil, status.Errorf(codes.InvalidArgument, errIdFromHex.Error())
@@ -125,7 +158,7 @@ func (srv *accountsService) DeleteAccount(ctx context.Context, in *accountspb.De
 		return nil, status.Errorf(codes.InvalidArgument, errDelete.Error())
 	}
 
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+	return new(emptypb.Empty), nil
 }
 
 func (srv *accountsService) Authenticate(ctx context.Context, in *accountspb.AuthenticateRequest) (*accountspb.AuthenticateReply, error) {
