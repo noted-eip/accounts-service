8c9580f4aeccd8e1f8c96a915bad2c5c7fb1a255
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-05-15 19:14:29 +0200

    feat(validators): improved validation checks

--- validators/validators.go
+++ validators/validators.go
@@ -3,15 +3,22 @@ package validator
 import (
 	"accounts-service/grpc/accountspb"
 
-	validation "github.com/go-ozzo/ozzo-validation"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
 //Basic check for new account information
-func ValidateCreateAccountRequest(newAccount *accountspb.CreateAccountRequest) error {
-	return validation.Errors{
-		"name":     validation.Validate(newAccount.Name, validation.Required, validation.Length(4, 20)),
-		"email":    validation.Validate(newAccount.Email, validation.Required, is.Email),
-		"password": validation.Validate(newAccount.Password, validation.Required, validation.Length(4, 20)),
-	}.Filter()
+func ValidateCreateAccountRequest(in *accountspb.CreateAccountRequest) error {
+	return validation.ValidateStruct(in,
+		validation.Field(&in.Name, validation.Required, validation.Length(4, 20)),
+		validation.Field(&in.Email, validation.Required, is.Email),
+		validation.Field(&in.Password, validation.Required, validation.Length(4, 20)),
+	)
+}
+
+func ValidateGetAccountRequest(in *accountspb.GetAccountRequest) error {
+	return validation.ValidateStruct(
+		validation.Field(&in.Id, validation.When(in.Id != "", validation.Required), is.UUID),
+		validation.Field(&in.Email, validation.When(in.Id != "", validation.Required), is.Email),
+	)
 }
