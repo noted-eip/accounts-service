56be41183413fb94631b1939358178a472abc282
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-05-28 14:19:42 +0200

    refactor: server configuration code

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ make codegen
 ```
 
 You can then build the project using the go toolchain.
+
+## Configuration
+
+| Env Name                           | Flag Name           | Default                     | Description                               |
+|------------------------------------|---------------------|-----------------------------|-------------------------------------------|
+| `ACCOUNTS_SERVICE_PORT`            | `--port`            | `3000`                      | The port the application shall listen on. |
+| `ACCOUNTS_SERVICE_ENV`             | `--env`             | `production`                | Either `production` or `development`.     |
+| `ACCOUNTS_SERVICE_DATABASE_URI`    | `--database-uri`    | `mongodb://localhost:27017` | Address of the Mongo database.            |
+| `ACCOUNTS_SERVICE_JWT_PRIVATE_KEY` | `--jwt-private-key` | -                           | Base64 encoded ed25519 private key.       |
--- main.go
+++ main.go
@@ -1,118 +1,39 @@
 package main
 
 import (
-	"context"
-	"crypto/ed25519"
-	"encoding/base64"
-	"fmt"
-	"net"
 	"os"
-	"time"
 
 	"accounts-service/auth"
-	"accounts-service/grpc/accountspb"
 	"accounts-service/models"
-	"accounts-service/models/mongo"
 
 	_ "github.com/joho/godotenv/autoload"
 
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/peer"
-	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
 	app = kingpin.New("accounts-service", "Accounts service for the Noted backend").DefaultEnvars()
 
-	// This is temporary. In the future we will want to have a KMS and eventually read the keys from
-	// a file or a mounted secret.
-	jwtPrivateKey = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
+	environment   = app.Flag("env", "either development or production").Default(envIsProd).Enum(envIsProd, envIsDev)
 	port          = app.Flag("port", "grpc server port").Default("3000").Int16()
 	databaseUri   = app.Flag("database-uri", "uri of the database").Default("mongodb://localhost:27017").String()
-	environment   = app.Flag("env", "either development or production").Default("production").String()
+	jwtPrivateKey = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
+)
+
+var (
+	envIsProd = "production"
+	envIsDev  = "development"
 )
 
 func main() {
 	app.Parse(os.Args[1:])
 
+	// We should remove this once the revamp of the models package is merged.
 	models.Init(*databaseUri)
-	authService := newAuthService()
-	logger := newLogger()
-	defer logger.Sync()
 
-	repo := mongo.NewAccountsRepository(logger)
-
-	srv := grpc.NewServer(
-		grpc.UnaryInterceptor(loggerUnaryInterceptor(logger)),
-	)
-
-	accSrv := accountsService{
-		logger: logger,
-		auth:   authService,
-		repo:   repo,
-	}
-	accountspb.RegisterAccountsServiceServer(srv, &accSrv)
-
-	startServer(srv)
-}
-
-func newAuthService() auth.Service {
-	rawKey, err := base64.StdEncoding.DecodeString(*jwtPrivateKey)
-	if err != nil {
-		panic("could not decode private key: " + err.Error())
-	}
-	return auth.NewService(ed25519.PrivateKey(rawKey))
-}
-
-func newLogger() *zap.SugaredLogger {
-	var logger *zap.Logger
-	var err error
-	if *environment == "development" {
-		logger, err = zap.NewDevelopment(zap.AddStacktrace(zapcore.FatalLevel))
-	} else {
-		logger, err = zap.NewProduction(zap.AddStacktrace(zapcore.FatalLevel))
-	}
-	if err != nil {
-		panic("unable to create logger: " + err.Error())
-	}
-	return logger.Sugar()
-}
-
-func startServer(srv *grpc.Server) {
-	lis, err := net.Listen("tcp", fmt.Sprint(":", *port))
-	if err != nil {
-		panic(err)
-	}
-	reflection.Register(srv)
-	if err := srv.Serve(lis); err != nil {
-		panic(err)
-	}
-}
-
-// Later this will be moved in an observability package.
-func loggerUnaryInterceptor(logger *zap.SugaredLogger) grpc.UnaryServerInterceptor {
-	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		start := time.Now()
-		res, err := auth.ForwardAuthMetadatathUnaryInterceptor(ctx, req, info, handler)
-		end := time.Now()
-
-		fields := []zapcore.Field{
-			zap.String("method", info.FullMethod),
-			zap.Duration("time", end.Sub(start)),
-		}
-		if peer, ok := peer.FromContext(ctx); ok {
-			fields = append(fields, zap.String("peer", peer.Addr.String()))
-		}
-		if err != nil {
-			fields = append(fields, zap.String("code", status.Code(err).String()), zap.String("error", err.Error()))
-			logger.Desugar().Warn("failed rpc", fields...)
-			return res, err
-		}
-		logger.Desugar().Info("rpc", fields...)
-		return res, err
-	}
+	s := &server{}
+	s.Init(grpc.ChainUnaryInterceptor(s.LoggerUnaryInterceptor, auth.ForwardAuthMetadatathUnaryInterceptor))
+	s.Run()
+	defer s.Close()
 }
