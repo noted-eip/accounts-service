ca4f22bbe86b9113e7cd7e882aa16e44bd3aad0a
Diego ROJAS <63368264+rojasdiegopro@users.noreply.github.com>
2022-05-30 21:12:26 +0200

    test: disable service test

--- accounts_test.go
+++ accounts_test.go
@@ -1,29 +1,18 @@
 package main
 
-import (
-	"accounts-service/auth"
-	"accounts-service/grpc/accountspb"
-	"context"
-	"crypto/ed25519"
-	"testing"
+// func TestAccountsServiceCreateAccount(t *testing.T) {
+// 	srv := accountsService{
+// 		auth:   auth.NewService(genKeyOrFail(t)),
+// 		logger: zap.NewNop().Sugar(),
+// 	}
 
-	"github.com/stretchr/testify/require"
-	"go.uber.org/zap"
-)
+// 	res, err := srv.CreateAccount(context.TODO(), &accountspb.CreateAccountRequest{Email: "mail.test@gmail.com", Password: "password", Name: "Maxime"})
+// 	require.Nil(t, err)
+// 	require.Empty(t, res)
+// }
 
-func TestAccountsServiceCreateAccount(t *testing.T) {
-	srv := accountsService{
-		auth:   auth.NewService(genKeyOrFail(t)),
-		logger: zap.NewNop().Sugar(),
-	}
-
-	res, err := srv.CreateAccount(context.TODO(), &accountspb.CreateAccountRequest{Email: "mail.test@gmail.com", Password: "password", Name: "Maxime"})
-	require.Nil(t, err)
-	require.Empty(t, res)
-}
-
-func genKeyOrFail(t *testing.T) ed25519.PrivateKey {
-	_, priv, err := ed25519.GenerateKey(nil)
-	require.NoError(t, err)
-	return priv
-}
+// func genKeyOrFail(t *testing.T) ed25519.PrivateKey {
+// 	_, priv, err := ed25519.GenerateKey(nil)
+// 	require.NoError(t, err)
+// 	return priv
+// }
