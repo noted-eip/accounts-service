b515744cf92f4acd05fa7104206660ab08458b60
maxime-dodin <maxime.dodin@epitech.eu>
2022-12-01 15:17:05 -0800

    feat:  put glabal variable from main package to a config package, to be called from the new controllers package

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"os"
 
 	"accounts-service/auth"
+	"accounts-service/config"
+	"accounts-service/controllers"
 
 	"google.golang.org/grpc"
 	"gopkg.in/alecthomas/kingpin.v2"
@@ -12,23 +14,24 @@ import (
 var (
 	app = kingpin.New("accounts-service", "Accounts service for the Noted backend").DefaultEnvars()
 
-	environment   = app.Flag("env", "either development or production").Default(envIsProd).Enum(envIsProd, envIsDev)
+	Environment   = app.Flag("env", "either development or production").Default(config.EnvIsProd).Enum(config.EnvIsProd, config.EnvIsDev)
 	port          = app.Flag("port", "grpc server port").Default("3000").Int16()
-	mongoUri      = app.Flag("mongo-uri", "address of the mongodb server").Default("mongodb://localhost:27017").String()
-	mongoDbName   = app.Flag("mongo-db-name", "name of the mongo database").Default("accounts-service").String()
-	jwtPrivateKey = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
-)
-
-var (
-	envIsProd = "production"
-	envIsDev  = "development"
+	MongoUri      = app.Flag("mongo-uri", "address of the mongodb server").Default("mongodb://localhost:27017").String()
+	MongoDbName   = app.Flag("mongo-db-name", "name of the mongo database").Default("accounts-service").String()
+	JwtPrivateKey = app.Flag("jwt-private-key", "base64 encoded ed25519 private key").Default("SGfCQAb05CtmhEesWxcrfXSQR6JjmEMeyjR7Mo21S60ZDW9VVTUuCvEMlGjlqiw4I/z8T11KqAXexvGIPiuffA==").String()
 )
 
 func main() {
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 
-	s := &server{}
+	s := &controllers.Server{}
+	config.Conf = &config.Config{
+		Environment:   Environment,
+		MongoUri:      MongoUri,
+		MongoDbName:   MongoDbName,
+		JwtPrivateKey: JwtPrivateKey,
+	}
 	s.Init(grpc.ChainUnaryInterceptor(s.LoggerUnaryInterceptor, auth.ForwardAuthMetadatathUnaryInterceptor))
-	s.Run()
+	s.Run(port)
 	defer s.Close()
 }
