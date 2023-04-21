ffa9adb64dc78886397c8b82af630e1cacaa0e49
maxime-dodin <maxime.dodin@epitech.eu>
2023-04-20 18:50:38 -0700

    feat: new environement variable gmail-super-secret, and store mailsAPI in accountsAPI

--- server.go
+++ server.go
@@ -5,7 +5,6 @@ import (
 	"accounts-service/models"
 	"accounts-service/models/mongo"
 	accountsv1 "accounts-service/protorepo/noted/accounts/v1"
-	mailingv1 "accounts-service/protorepo/noted/mailing/v1"
 	"context"
 	"crypto/ed25519"
 	"encoding/base64"
@@ -33,8 +32,6 @@ type server struct {
 
 	accountsService accountsv1.AccountsAPIServer
 
-	mailingService mailingv1.MailingAPIServer
-
 	grpcServer *grpc.Server
 }
 
@@ -44,7 +41,6 @@ func (s *server) Init(opt ...grpc.ServerOption) {
 	s.initAuthService()
 	s.initRepositories()
 	s.initAccountsAPI()
-	s.initMailingAPI()
 	s.initGrpcServer(opt...)
 }
 
@@ -118,24 +114,23 @@ func (s *server) initRepositories() {
 }
 
 func (s *server) initAccountsAPI() {
+	mail := mailingAPI{
+		logger: s.logger,
+		repo:   s.accountsRepository,
+		secret: *gmailSuperSecret,
+	}
+
 	s.accountsService = &accountsAPI{
 		auth:   s.authService,
 		logger: s.logger,
 		repo:   s.accountsRepository,
-	}
-}
-
-func (s *server) initMailingAPI() {
-	s.mailingService = &mailingAPI{
-		logger: s.logger,
-		repo:   s.accountsRepository,
+		mail:   mail,
 	}
 }
 
 func (s *server) initGrpcServer(opt ...grpc.ServerOption) {
 	s.grpcServer = grpc.NewServer(opt...)
 	accountsv1.RegisterAccountsAPIServer(s.grpcServer, s.accountsService)
-	mailingv1.RegisterMailingAPIServer(s.grpcServer, s.mailingService)
 }
 
 func must(err error, msg string) {
