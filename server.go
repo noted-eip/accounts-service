9d98474da99ad81351d3d0584088b25049313b52
maxime-dodin <maxime.dodin@epitech.eu>
2023-05-09 21:49:36 -0700

    feat: init pending-accounts and store env value in accountsAPI

--- server.go
+++ server.go
@@ -29,7 +29,8 @@ type server struct {
 
 	mongoDB *mongo.Database
 
-	accountsRepository models.AccountsRepository
+	accountsRepository        models.AccountsRepository
+	pendingAccountsRepository models.PendingAccountsRepository
 
 	accountsService accountsv1.AccountsAPIServer
 	noteService     *communication.NoteServiceClient
@@ -126,14 +127,16 @@ func (s *server) initRepositories() {
 	s.mongoDB, err = mongo.NewDatabase(context.Background(), *mongoUri, *mongoDbName, s.logger)
 	must(err, "could not instantiate mongo database")
 	s.accountsRepository = mongo.NewAccountsRepository(s.mongoDB.DB, s.logger)
+	s.pendingAccountsRepository = mongo.NewPendingAccountsRepository(s.mongoDB.DB, s.logger)
 }
 
 func (s *server) initAccountsAPI() {
 
 	mailService := mailingAPI{
-		logger: s.logger,
-		repo:   s.accountsRepository,
-		secret: *gmailSuperSecret,
+		logger:      s.logger,
+		repo:        s.accountsRepository,
+		pendingRepo: s.pendingAccountsRepository,
+		secret:      *gmailSuperSecret,
 	}
 
 	s.accountsService = &accountsAPI{
@@ -142,6 +145,8 @@ func (s *server) initAccountsAPI() {
 		auth:        s.authService,
 		logger:      s.logger,
 		repo:        s.accountsRepository,
+		pendingRepo: s.pendingAccountsRepository,
+		env:         environment,
 	}
 }
 
