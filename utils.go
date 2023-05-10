f7c81dd0d31f5cac285bdd9fa2ccc106126e3d70
maxime-dodin <maxime.dodin@epitech.eu>
2023-05-09 22:26:22 -0700

    fix: update unit test

--- utils.go
+++ utils.go
@@ -44,6 +44,8 @@ func newTestUtilsOrDie(t *testing.T) *testUtils {
 	randomAlphanumeric, err := nanoid.CustomASCII("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 8)
 	require.NoError(t, err)
 
+	env := "production"
+
 	return &testUtils{
 		logger:             logger,
 		auth:               auth,
@@ -55,6 +57,7 @@ func newTestUtilsOrDie(t *testing.T) *testUtils {
 			auth:   auth,
 			logger: logger,
 			repo:   accountsRepository,
+			env:    &env,
 		},
 	}
 }
